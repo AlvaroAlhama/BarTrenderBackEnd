1bc3292401da7b884c31eaf2f2647e7cca5d62fe
enrreigut <enrreigut@alum.us.es>
2021-03-21 16:53:07 +0100

    ADDED SERIALIZERS AND GET DISCOUNTS TO API:
    Another API method created and added serializers so objects can be
    passed.

--- barTrenderBackEnd/establishments/urls.py
+++ barTrenderBackEnd/establishments/urls.py
@@ -2,5 +2,6 @@ from django.urls import path,include
 from .views import *
 
 urlpatterns = [
-    path('<int:establishment_id>/discounts/<int:discount_id>/getDiscountQR', Discounts.as_view()),
+    path('<int:establishment_id>/discounts/get/', Discounts.as_view()),
+    path('<int:establishment_id>/discounts/<int:discount_id>/getQR/', DiscountsQR.as_view()),
 ]
