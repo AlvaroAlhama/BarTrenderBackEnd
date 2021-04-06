e597107d0692a5e981b15b9569f39b02e2b9326c
Álvaro Aguilar Alhama <alvagualh@alum.us.es>
2021-04-06 19:39:14 +0200

    Update tests_model.py

--- barTrenderBackEnd/establishments/tests/tests_model.py
+++ barTrenderBackEnd/establishments/tests/tests_model.py
@@ -198,9 +198,8 @@ class EstablishmentsTestCase(TransactionTestCase):
                 establishment.save()
 
         new_count = Establishment.objects.all().count()
-        print(str(context.exception))
 
-        self.assertTrue('“%(value)s” value must be an integer.' in str(context.exception))
+        self.assertTrue('value must be an integer.' in str(context.exception))
         self.assertTrue(new_count - prev_count == 0)
 
     def test_invalid_establishment_phone_blank(self):
